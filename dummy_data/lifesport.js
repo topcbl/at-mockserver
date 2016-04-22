0f3e10dc61cf09ba5b8c1b097591623c4162134d
at-longcb <longcb@asiantech.vn>
2016-04-22 14:53:01 +0700

    Reformated code.

--- dummy_data/lifesport.js
+++ dummy_data/lifesport.js
@@ -5,7 +5,7 @@ var faker = require("faker");
 var _ = require("lodash");
 const NUMBER_OF_USER = 100;
 const NUMBER_OF_EVENT = 5;
-const NUMBER_OF_SPORT = 12;
+const NUMBER_OF_SPORT = 12
 const NUMBER_OF_FACILITIES = 20;
 const NUMBER_OF_CITIES = 200;
 
@@ -156,7 +156,7 @@ module.exports.getParticipantFee = function getParticipantFee() {
     }
 }
 
-function getTags() {
+module.exports.getTags = function getTags() {
     var faker = require("faker");
     var _ = require("lodash");
     return {
@@ -169,7 +169,7 @@ function getTags() {
     }
 }
 
-function getEventTags() {
+module.exports.getEventTags = function getEventTags() {
     return {
         event_tags: _.times(20, function (n) {
             return {
@@ -181,7 +181,7 @@ function getEventTags() {
     }
 }
 
-function getFavoriteEvents() {
+module.exports.getFavoriteEvents = function getFavoriteEvents() {
     return {
         favourites_events: _.times(100, function (n) {
             return {
@@ -271,7 +271,7 @@ module.exports.getWaitingEvents = function getWaitingEvents() {
     }
 }
 
-function getFacilitiesSports() {
+module.exports.getFacilitiesSports = function getFacilitiesSports() {
     return {
         facility_sports: _.times(NUMBER_OF_FACILITIES, function (n) {
             return {
@@ -284,205 +284,54 @@ function getFacilitiesSports() {
 }
 
 module.exports.getFacilities = function getFacilities() {
-    return  {
-            "total": 6,
-            "per_page": 6,
-            "has_next_page": false,
-            "facilities": [
-                {
-                    "id": 4,
-                    "distance": "0.013",
-                    "name": "Duy tan 4",
-                    "image_url": "https://Schmeler.info/culpa-est-facilis-quia-impedit.html",
-                    "address": "03487 Hazel Ways Suite 988\nNorth Stephanie, LA 10462"
-                },
-                {
-                    "id": 3,
-                    "distance": "0.015",
-                    "name": "Duy tan 3",
-                    "image_url": "https://Stoltenberg.biz/odit-vel-culpa-eius-distinctio-quia.html",
-                    "address": "57402 Davonte Canyon\nMartinland, CO 65201-1584"
-                },
-                {
-                    "id": 1,
-                    "distance": "0.065",
-                    "name": "Duy tan 1",
-                    "image_url": "http://Russel.com/natus-minima-numquam-voluptatum-et-error-enim.html",
-                    "address": "685 Maxime Skyway\nLake Mavis, CT 31772-2357"
-                },
-                {
-                    "id": 2,
-                    "distance": "0.084",
-                    "name": "Duy tan 2",
-                    "image_url": "http://Schaefer.com/similique-eaque-adipisci-cum-et-enim-dolorem-quos",
-                    "address": "2990 Herman Islands\nHanebury, FL 64043"
-                },
-                {
-                    "id": 5,
-                    "distance": "0.188",
-                    "name": "Noi that go Nui ThanhDa Nang",
-                    "image_url": "https://www.Tromp.org/aperiam-dignissimos-fugit-sit-maxime-in-maiores-earum",
-                    "address": "46 Nui Thanh"
-                },
-                {
-                    "id": 6,
-                    "distance": "4.626",
-                    "name": "Yet Kieu Da Nang",
-                    "image_url": "http://Jacobs.com/et-voluptatum-aliquid-natus-illo-inventore-pariatur-et",
-                    "address": "34644 Odell Court\nEast Terryport, IL 45353"
-                }
-            ]
-    }
-}
-
-function getParticipants() {
     return {
-        participants: _.times(100, function (n) {
-            return {
-                id: n,
-                user_id: n,
-                event_id: n,
-                status: +faker.random.boolean()
+        "total": 6,
+        "per_page": 6,
+        "has_next_page": false,
+        "facilities": [
+            {
+                "id": 4,
+                "distance": "0.013",
+                "name": "Duy tan 4",
+                "image_url": "https://Schmeler.info/culpa-est-facilis-quia-impedit.html",
+                "address": "03487 Hazel Ways Suite 988\nNorth Stephanie, LA 10462"
+            },
+            {
+                "id": 3,
+                "distance": "0.015",
+                "name": "Duy tan 3",
+                "image_url": "https://Stoltenberg.biz/odit-vel-culpa-eius-distinctio-quia.html",
+                "address": "57402 Davonte Canyon\nMartinland, CO 65201-1584"
+            },
+            {
+                "id": 1,
+                "distance": "0.065",
+                "name": "Duy tan 1",
+                "image_url": "http://Russel.com/natus-minima-numquam-voluptatum-et-error-enim.html",
+                "address": "685 Maxime Skyway\nLake Mavis, CT 31772-2357"
+            },
+            {
+                "id": 2,
+                "distance": "0.084",
+                "name": "Duy tan 2",
+                "image_url": "http://Schaefer.com/similique-eaque-adipisci-cum-et-enim-dolorem-quos",
+                "address": "2990 Herman Islands\nHanebury, FL 64043"
+            },
+            {
+                "id": 5,
+                "distance": "0.188",
+                "name": "Noi that go Nui ThanhDa Nang",
+                "image_url": "https://www.Tromp.org/aperiam-dignissimos-fugit-sit-maxime-in-maiores-earum",
+                "address": "46 Nui Thanh"
+            },
+            {
+                "id": 6,
+                "distance": "4.626",
+                "name": "Yet Kieu Da Nang",
+                "image_url": "http://Jacobs.com/et-voluptatum-aliquid-natus-illo-inventore-pariatur-et",
+                "address": "34644 Odell Court\nEast Terryport, IL 45353"
             }
-        })
-    }
-}
-
-function getCheckList() {
-    return {
-        checklists: _.times(100, function (n) {
-            return {
-                id: n,
-                product_id: n,
-                user_id: n
-            }
-        })
-    }
-}
-
-function getECAdmin() {
-    return {
-        ec_admins: _.times(100, function (n) {
-            return {
-                id: n,
-                company_id: n,
-                email: faker.internet.email(),
-                password: "123abc",
-                name: faker.name.findName(),
-                lastest_login: faker.date.recent(),
-                is_active: faker.random.boolean()
-            }
-        })
-    }
-}
-
-function getProducts() {
-    return {
-        products: _.times(100, function (n) {
-            return {
-                id: n,
-                ec_admin_id: n,
-                sport_id: n,
-                category_id: n,
-                title: faker.lorem.words(),
-                description: faker.lorem.words(),
-                reference_price: faker.random.number() % 100
-            }
-        })
-    }
-}
-
-function getCategories() {
-    return {
-        categories: _.times(100, function (n) {
-            return {
-                id: n,
-                category_name: faker.lorem.words()
-            }
-        })
-    }
-}
-
-function getCompanies() {
-    return {
-        companies: _.times(100, function (n) {
-            return {
-                id: n,
-                company_name: faker.company.companyName()
-            }
-        })
-    }
-}
-
-function getCarts() {
-    return {
-        carts: _.times(100, function (n) {
-            return {
-                id: n,
-                user_id: n,
-                product_detail_id: n
-            }
-        })
-    }
-}
-
-function getProductDetails() {
-    return {
-        product_details: _.times(100, function (n) {
-            return {
-                id_recruitment: n,
-                product_id: n,
-                size: "S",
-                color: "Red",
-                price: faker.random.number() % 1000,
-                inventory_number: faker.random.number() % 100
-            }
-        })
-    }
-}
-
-function getProductImages() {
-    return {
-        product_images: _.times(100, function (n) {
-            return {
-                id: n,
-                image_url: faker.image.sports(),
-                variation_id: n
-            }
-        })
-    }
-}
-
-function getOrders() {
-    return {
-        orders: _.times(100, function (n) {
-            return {
-                id: n,
-                user_id: n,
-                payment_id: n,
-                subtotal: faker.random.number() % 1000,
-                shipping_address: faker.random.number() % 1000,
-                payment_method: "Paypal",
-                status: faker.random.number() % 4
-            }
-        })
-    }
-}
-
-function getOrderDetails() {
-    var faker = require("faker");
-    var _ = require("lodash");
-    return {
-        order_details: _.times(100, function (n) {
-            return {
-                id: n,
-                product_detail_id: n,
-                order_id: n,
-                price: faker.random.number() % 1000,
-                order_date: faker.date.past(),
-                order_status: faker.random.number() % 3
-            }
-        })
+        ]
     }
 }
 
